8c9eeb8c4f70b39f5e32212785fb7f559c78fd29
MahidunNobi <mdmahidunnobi@gmail.com>
2025-09-28 11:04:24 +0600

    first commit

---
+++ src/app/(dashboard-layout)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      DashboardLayout
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/app/(public-layout)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+const PublicLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      PublicLayout
+      {children}
+    </div>
+  );
+};
+
+export default PublicLayout;
