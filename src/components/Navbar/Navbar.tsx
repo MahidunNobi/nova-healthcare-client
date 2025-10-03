fe7f8de1f0f2f87f2b80e31f98c7d6aff32f065f
MahidunNobi <mdmahidunnobi@gmail.com>
2025-10-03 11:58:24 +0600

    Add: NavLinks

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import Link from "next/link";
+import navLinks from "@/lib/NavLinks";
 
 const Navbar = () => {
   return (
     <div className=" py-6 px-2 border-b border-gray-200">
       <div className="container mx-auto">
         {/* Left Container */}
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-12">
           <Logo />
-          <div>
-            <Link href="#" className="text-lg text-gray-800">
-              Consultation
-            </Link>
+          <div className="space-x-6">
+            {navLinks.map((li) => (
+              <Link
+                key={li.id}
+                href={li.link}
+                className="text-lg text-gray-800"
+              >
+                {li.label}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/lib/NavLinks.ts
@@ -0,0 +1,34 @@
+const navLinks = [
+  {
+    id: 1,
+    link: "",
+    label: "Cunsultation",
+  },
+  {
+    id: 2,
+    link: "",
+    label: "Order Medicine",
+  },
+  {
+    id: 3,
+    link: "",
+    label: "Home Diagnostic",
+  },
+  {
+    id: 4,
+    link: "",
+    label: "Health Plan",
+  },
+  {
+    id: 5,
+    link: "",
+    label: "Blogs",
+  },
+  {
+    id: 6,
+    link: "",
+    label: "More",
+  },
+];
+
+export default navLinks
