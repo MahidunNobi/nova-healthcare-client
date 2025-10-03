b17e9a74c16539654dc200112dedb213d5e25d2a
MahidunNobi <mdmahidunnobi@gmail.com>
2025-10-03 11:52:27 +0600

    Add: Font

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../Logo/Logo";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className=" py-6 px-2 border-b border-gray-200">
       <div className="container mx-auto">
         {/* Left Container */}
-        <div>
+        <div className="flex items-center gap-6">
           <Logo />
+          <div>
+            <Link href="#" className="text-lg text-gray-800">
+              Consultation
+            </Link>
+          </div>
         </div>
       </div>
     </div>
