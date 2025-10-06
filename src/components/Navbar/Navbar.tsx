040f471a3ccd1fc81ab5aee19f7a5016f3b3bf51
MahidunNobi <mdmahidunnobi@gmail.com>
2025-10-06 21:04:47 +0600

    Custome-Button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,16 +7,16 @@ import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <div className=" py-6 px-2 border-b border-gray-200">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex items-center justify-between">
         {/* Left Container */}
         <div className="flex items-center gap-12">
           <Logo />
-          <div className="space-x-6">
+          <div className="space-x-6 hidden lg:block">
             {navLinks.map((li) => (
               <Link
                 key={li.id}
                 href={li.link}
-                className="text-lg text-gray-800"
+                className="text- text-gray-800 hover:primary"
               >
                 {li.label}
               </Link>
