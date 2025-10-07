1ddc6247e3d747764fc4c373444c90ec888122c4
MahidunNobi <mdmahidunnobi@gmail.com>
2025-10-07 11:04:21 +0600

    Add-func: Auto Slider

--- src/components/Home/HeroSlider.tsx
+++ src/components/Home/HeroSlider.tsx
@@ -7,27 +7,41 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 import Container from "../Container/Container";
+import Image from "next/image";
 
 const HeroSlider = () => {
   return (
     <Container>
-      <div className="my-8">
+      <div className="my-4">
         <Swiper
           pagination={true}
-          modules={[Pagination]}
-          className="mySwiper min-h-14"
+          modules={[Pagination, Autoplay]}
+          className="mySwiper min-h-14 rounded-lg"
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/hero1.jpg"
+              alt="Hero Image One"
+              width={3018}
+              height={766}
+              className="object-cover h-full w-auto"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/hero2.jpg"
+              alt="Hero Image One"
+              width={3018}
+              height={766}
+              className="object-cover h-full w-auto"
+            />
+          </SwiperSlide>
         </Swiper>
       </div>
     </Container>
