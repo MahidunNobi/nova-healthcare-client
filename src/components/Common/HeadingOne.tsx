acddb0ec5c73c99c3ff7ba69ae534237b7f26961
MahidunNobi <mdmahidunnobi@gmail.com>
2025-11-12 12:20:51 +0600

    Add Class to HeadingOne Component

--- src/components/Common/HeadingOne.tsx
+++ src/components/Common/HeadingOne.tsx
@@ -1,8 +1,16 @@
 import React, { ReactNode } from "react";
 
-const HeadingOne = ({ children }: { children: ReactNode }) => {
+const HeadingOne = ({
+  children,
+  addClass,
+}: {
+  children: ReactNode;
+  addClass?: string;
+}) => {
   return (
-    <h1 className="text-4xl md:text-5xl font-extrabold leading-12 md:leading-16">
+    <h1
+      className={`text-4xl md:text-5xl font-extrabold leading-12 md:leading-16 ${addClass}`}
+    >
       {children}
     </h1>
   );
